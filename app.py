084a22dbec5c40a687e3a861f0007ff1a12b380a
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-12-24 20:44:50 -0500

    Update image URL to point to new repository

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Clash Royale App", layout="centered")
 from urllib.request import urlopen
 from PIL import Image
 
-url = "https://raw.githubusercontent.com/username/repo/branch/colored-logo.png"
+url = "https://raw.githubusercontent.com/SakritUser123/ClashRoyaleDeckGenerator/refs/heads/main/colored-logo.png"
 image = Image.open(urlopen(url))
 
 st.image(image, width=400)
