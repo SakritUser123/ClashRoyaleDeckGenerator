2ce30c7febf2962ead38bb1678d945e28c70d1f8
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-12-24 19:57:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import streamlit as st
 st.header(" Clash Royale Deck Generator AI")
 
-playerID = st.text_input("Enter Player ID Here: ", "Ex.#ABC123DEF45")
 
-predict_decks = st.button("Predict Deck!", on_click= None)
+playerID = st.text_input("Enter Player ID: ", key="player_id_key")
+
+
+if st.button("Predict!"):
+   
+    if playerID:
+        st.write(f"You submitted: {playerID}")
+    else:
+        st.write("Please enter ID before submitting.")
 rating = st.slider("Rate The ouput of the model", min_value=0, max_value=10, value=1, step=1)
 st.write(f"Your rating is {rating}")
