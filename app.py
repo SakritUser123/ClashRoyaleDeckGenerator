e74e7c208a44844bf404ad18e31c0314cb971802
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-12-24 20:37:16 -0500

    Update logo source to use GitHub URL

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ st.markdown("""
     </style>
 
     <div id="splash">
-        <img src="colored-logo.png">
+        <img src="https://github.com/SakritUser123/ClashRoyaleDeckGenerator/edit/main/colored-logo.png">
     </div>
 """, unsafe_allow_html=True)
 
