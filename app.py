ee0dabc172f1f839a2546845f9db6dfd709d5fc7
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-12-24 20:00:27 -0500

    Remove custom styles for slider in app.py
    Removed custom slider styles from markdown.

--- app.py
+++ app.py
@@ -13,19 +13,6 @@ if st.button("Predict!"):
         st.write("Please enter ID before submitting.")
 
 st.header("Rate the output of the model")
-st.markdown("""
-<style>
-/* Target the track of the slider */
-div.stSlider > div[data-baseweb="slider"] > div > div {
-    background: linear-gradient(to right, #FF0000 0%, #FFA500 25%, #FFFF00 50%, #008000 75%, #0000FF 100%);
-}
 
-/* Target the thumb (the movable circle) */
-div.stSlider > div[data-baseweb="slider"] > div > div > div > div {
-    background-color: #FFFFFF; /* White thumb */
-    border: 1px solid #000000;
-}
-</style>
-""", unsafe_allow_html=True)
 rating = st.slider("Rate The ouput of the model", min_value=0, max_value=10, value=1, step=1)
 st.write(f"Your rating is {rating}")
