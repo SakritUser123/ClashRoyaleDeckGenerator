f870fed6d47a28fe65f8b62f9404e345ccb66324
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-12-24 19:44:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
+st.header(" Clash Royale Deck Generator AI")
+
+user_input = st.text_input("Enter Player ID Here: ", "Ex.#ABC123DEF45")
+
 rating = st.slider("Rate The ouput of the model", min_value=0, max_value=10, value=1, step=1)
 st.write(f"Your rating is {rating}")
