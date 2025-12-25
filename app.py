a0c9881df957acaf415b4abea6bd3390cdcac251
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-12-24 19:58:34 -0500

    Add rating slider for model output

--- app.py
+++ app.py
@@ -11,5 +11,7 @@ if st.button("Predict!"):
         st.write(f"You submitted: {playerID}")
     else:
         st.write("Please enter ID before submitting.")
+
+st.header("Rate the output of the model")
 rating = st.slider("Rate The ouput of the model", min_value=0, max_value=10, value=1, step=1)
 st.write(f"Your rating is {rating}")
