83e21922e1fbed9f6e003fc294c2daf03f28a732
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-12-24 19:54:53 -0500

    Refactor user input variable and add predict button

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 st.header(" Clash Royale Deck Generator AI")
 
-user_input = st.text_input("Enter Player ID Here: ", "Ex.#ABC123DEF45")
+playerID = st.text_input("Enter Player ID Here: ", "Ex.#ABC123DEF45")
 
+predict_decks = st.button("Predict Deck!", on_click= None)
 rating = st.slider("Rate The ouput of the model", min_value=0, max_value=10, value=1, step=1)
 st.write(f"Your rating is {rating}")
