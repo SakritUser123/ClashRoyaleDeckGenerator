3e0f17e50a8fef2c3afc9910b08ce9efe21e09f8
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-12-24 20:40:24 -0500

    Display image from URL in the app
    Added functionality to display an image from a URL.

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ playerID = st.text_input("Enter Player ID: ", key="player_id_key")
 
 st.set_page_config(page_title="Clash Royale App", layout="centered")
 
+
+from urllib.request import urlopen
+from PIL import Image
+
+url = "https://raw.githubusercontent.com/username/repo/branch/colored-logo.png"
+image = Image.open(urlopen(url))
+
+st.image(image, width=400)
+
 # Splash screen HTML + CSS
 st.markdown("""
     <style>
